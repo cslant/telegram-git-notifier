087de366f55dbd188061b20bc781c2d515a25e97
Tan Nguyen <tannp27@gmail.com>
2023-09-23 15:02:29 +0700

    update constants

--- src/Constants/EventConstant.php
+++ src/Constants/EventConstant.php
@@ -6,6 +6,11 @@ class EventConstant
 {
     public const DEFAULT_PLATFORM = 'github';
 
+    public const WEBHOOK_EVENT_HEADER = [
+        'github' => 'HTTP_X_GITHUB_EVENT',
+        'gitlab' => 'HTTP_X_GITLAB_EVENT',
+    ];
+
     public const EVENT_PREFIX = SettingConstant::SETTING_CUSTOM_EVENTS . '.evt.';
 
     public const GITHUB_EVENT_SEPARATOR = 'gh.';
@@ -15,9 +20,4 @@ class EventConstant
     public const EVENT_HAS_ACTION_SEPARATOR = 'atc.';
 
     public const EVENT_UPDATE_SEPARATOR = '.eus';
-
-    public const WEBHOOK_EVENT_HEADER = [
-        'github' => 'HTTP_X_GITHUB_EVENT',
-        'gitlab' => 'HTTP_X_GITLAB_EVENT',
-    ];
 }
