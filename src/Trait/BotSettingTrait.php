4c8ec128c113750fb26481724f9e89314c76df45
Tan Nguyen <tannp27@gmail.com>
2023-10-11 21:42:00 +0700

    update setting service

---
+++ src/Trait/BotSettingTrait.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace LbilTech\TelegramGitNotifier\Trait;
+
+use LbilTech\TelegramGitNotifier\Constants\SettingConstant;
+
+trait BotSettingTrait
+{
+    public function settingHandle(?string $view = null): void
+    {
+        $this->sendMessage(
+            view($view ?? config('telegram-git-notifier.view.tools.setting')),
+            ['reply_markup' => $this->settingMarkup()]
+        );
+    }
+
+    public function settingMarkup(): array
+    {
+        $markup = [
+            [
+                $this->telegram->buildInlineKeyBoardButton(
+                    $this->setting[SettingConstant::T_IS_NOTIFIED]
+                        ? '✅ Allow notifications'
+                        : 'Allow notifications',
+                    '',
+                    SettingConstant::SETTING_IS_NOTIFIED
+                ),
+            ],
+            [
+                $this->telegram->buildInlineKeyBoardButton(
+                    $this->setting[SettingConstant::T_ALL_EVENTS_NOTIFICATION]
+                        ? '✅ Enable All Events Notify'
+                        : 'Enable All Events Notify',
+                    '',
+                    SettingConstant::SETTING_ALL_EVENTS_NOTIFY
+                ),
+            ]
+        ];
+
+        $markup = $this->customEventMarkup($markup);
+
+        $markup[] = [
+            $this->telegram->buildInlineKeyBoardButton(
+                '🔙 Back to menu',
+                '',
+                SettingConstant::SETTING_BACK . 'menu'
+            ),
+        ];
+
+        return $markup;
+    }
+
+    public function customEventMarkup(array $markup): array
+    {
+        if (!$this->setting[SettingConstant::T_ALL_EVENTS_NOTIFICATION]) {
+            $markup[] = [
+                $this->telegram->buildInlineKeyBoardButton(
+                    '🦑 Custom github events',
+                    '',
+                    SettingConstant::SETTING_GITHUB_EVENTS
+                ),
+                $this->telegram->buildInlineKeyBoardButton(
+                    '🦊 Custom gitlab events',
+                    '',
+                    SettingConstant::SETTING_GITLAB_EVENTS
+                ),
+            ];
+        }
+
+        return $markup;
+    }
+}
