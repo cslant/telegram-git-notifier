22f22386b19243ebe3044edf8da053ef2ce3493b
Tan Nguyen <tannp27@gmail.com>
2023-11-02 14:32:17 +0700

    update bash install and config test

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Setting up config files for Telegram Git Notifier..."
+
+mkdir -p storage/json/tgn
+
 json_files=(
     "github-events.json"
     "gitlab-events.json"
@@ -9,9 +13,12 @@ json_files=(
 for file in "${json_files[@]}"; do
     if [ ! -f "storage/json/tgn/$file" ]; then
         cp "./config/jsons/$file" "storage/json/tgn/$file"
+        echo "Created storage/json/tgn/$file"
     fi
 done
 
 if [[ "$(uname -s -r)" == *"Linux"* && "$(cat /etc/os-release)" == *"Ubuntu"* ]]; then
     chmod 777 storage/json/tgn/*.json
 fi
+
+echo "Telegram Git Notifier config files are ready!"
