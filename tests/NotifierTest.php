6685785137ddc1fb5ea6fba7c2cdd11242123506
tanhongit <tanhongit@users.noreply.github.com>
2024-01-12 09:42:25 +0000

    Fix styling

--- tests/NotifierTest.php
+++ tests/NotifierTest.php
@@ -23,6 +23,6 @@ it('can parse notification chat IDs', function () {
         ->and($chatThreadMapping)->toHaveKey('-1008168942527')
         ->and($chatThreadMapping['-1008168942527'])->toBeArray()
         ->and($chatThreadMapping['-1008168942527'])->toBe([
-            0 => '46', 1 => '2'
+            0 => '46', 1 => '2',
         ]);
 });
